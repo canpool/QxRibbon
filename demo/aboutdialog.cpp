canpool/QxRibbon

commit 329725af9b620acd5ac809d759af4e6d83c36342

add AboutDialog
Signed-off-by: canpool <canpool@163.com>

demo/aboutdialog.cpp
@@ -1,0 +1,99 @@
+#include "aboutdialog.h"
+
+#include <QDialogButtonBox>
+#include <QGridLayout>
+#include <QLabel>
+#include <QPushButton>
+#include <QDateTime>
+#include <time.h>
+
+static time_t getDateFromMacro(const char *time)
+{
+    char s_month[5];
+    int month, day, year;
+    struct tm t;
+    memset(&t, 0, sizeof(t));
+    static const char month_names[] = "JanFebMarAprMayJunJulAugSepOctNovDec";
+
+    sscanf_s(time, "%s %d %d", s_month, (unsigned int)sizeof(s_month) - 1, &day, &year);
+
+    month = (strstr(month_names, s_month) - month_names) / 3;
+
+    t.tm_mon = month;   // [0,11]
+    t.tm_mday = day;
+    t.tm_year = year - 1900;
+    t.tm_isdst = -1;
+
+    return mktime(&t);
+}
+
+static QString compilerString()
+{
+#if defined(Q_CC_CLANG) // must be before GNU, because clang claims to be GNU too
+    QString isAppleString;
+#if defined(__apple_build_version__) // Apple clang has other version numbers
+    isAppleString = QLatin1String(" (Apple)");
+#endif
+    return QLatin1String("Clang " ) + QString::number(__clang_major__) + QLatin1Char('.')
+            + QString::number(__clang_minor__) + isAppleString;
+#elif defined(Q_CC_GNU)
+    return QLatin1String("GCC " ) + QLatin1String(__VERSION__);
+#elif defined(Q_CC_MSVC)
+    if (_MSC_VER > 1999)
+        return QLatin1String("MSVC <unknown>");
+    if (_MSC_VER >= 1920)
+        return QLatin1String("MSVC 2019");
+    if (_MSC_VER >= 1910)
+        return QLatin1String("MSVC 2017");
+    if (_MSC_VER >= 1900)
+        return QLatin1String("MSVC 2015");
+#endif
+    return QLatin1String("<unknown compiler>");
+}
+
+AboutDialog::AboutDialog(const QString &softlogo, const QString &softname, const QString &softver,
+                         const QString &softdesc, QWidget *parent)
+    : QDialog(parent)
+{
+    setWindowTitle(tr("About %1").arg(softname));
+
+    QGridLayout *layout = new QGridLayout(this);
+    layout->setSizeConstraint(QLayout::SetFixedSize);
+
+    QString versionString = tr("%1 %2").arg(softname, softver);
+    QDateTime dt = QDateTime::fromTime_t((uint)getDateFromMacro(__DATE__));
+    QString buildDateInfo = tr("<br/>Built on %1 %2<br/>").arg(dt.toString("yyyy-MM-dd"), __TIME__);
+    QString buildCompatibilityString = tr("Based on Qt %1 (%2, %3 bit)").arg(QLatin1String(qVersion()),
+                                                                             compilerString(),
+                                                                             QString::number(QSysInfo::WordSize));
+    const QString description = tr(
+        "<h3>%1</h3>"
+        "%2<br/>"
+        "%3"
+        "<br/>"
+        "%4")
+        .arg(versionString,
+             buildCompatibilityString,
+             buildDateInfo,
+             softdesc);
+
+    QLabel *copyRightLabel = new QLabel(description);
+    copyRightLabel->setWordWrap(true);
+    copyRightLabel->setOpenExternalLinks(true);
+    copyRightLabel->setTextInteractionFlags(Qt::TextBrowserInteraction);
+
+    QDialogButtonBox *buttonBox = new QDialogButtonBox(QDialogButtonBox::Close);
+    QPushButton *closeButton = buttonBox->button(QDialogButtonBox::Close);
+    buttonBox->addButton(closeButton, QDialogButtonBox::ButtonRole(QDialogButtonBox::RejectRole | QDialogButtonBox::AcceptRole));
+    connect(buttonBox , &QDialogButtonBox::rejected, this, &QDialog::reject);
+
+    QLabel *logoLabel = new QLabel;
+    logoLabel->setPixmap(softlogo);
+    layout->addWidget(logoLabel , 0, 0, 1, 1);
+    layout->addWidget(copyRightLabel, 0, 1, 4, 4);
+    layout->addWidget(buttonBox, 4, 0, 1, 5);
+}
+
+AboutDialog::~AboutDialog()
+{
+}

demo/CMakeLists.txt
@@ -13,6 +13,8 @@ find_package(Qt${QT_VERSION_MAJOR} COMPONENTS Core Gui Widgets REQUIRED)
 set(PROJECT_SOURCES
     mainwindow.h
     mainwindow.cpp
+    aboutdialog.h
+    aboutdialog.cpp
     main.cpp
     demo.qrc
 )

demo/aboutdialog.h
@@ -1,0 +1,15 @@
+#ifndef ABOUTDIALOG_H
+#define ABOUTDIALOG_H
+
+#include <QDialog>
+
+class AboutDialog : public QDialog
+{
+    Q_OBJECT
+public:
+    explicit AboutDialog(const QString &softlogo, const QString &softname, const QString &softver,
+                         const QString &softdesc, QWidget *parent = nullptr);
+    virtual ~AboutDialog();
+};
+
+#endif // ABOUTDIALOG_H

demo/demo.pro
@@ -14,10 +14,12 @@ DESTDIR = $${OUT_ROOT}/bin
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
+    aboutdialog.cpp \
     main.cpp \
     mainwindow.cpp
 
 HEADERS += \
+    aboutdialog.h \
     mainwindow.h
 
 include($$PWD/../common.pri)

demo/mainwindow.cpp
@@ -37,6 +37,8 @@
 #include <QXmlStreamWriter>
 #include <QDialog>
 
+#include "aboutdialog.h"
+
 QX_USE_NAMESPACE
 
 #define PRINT_COST_START()                                                                                             \
@@ -186,12 +188,15 @@ void MainWindow::onActionCustomizeAndSaveTriggered(bool b)
 
 void MainWindow::onActionHelpTriggered()
 {
-    QMessageBox::information(this, tr("infomation"),
-                             tr("\n ==============="
-                                "\n RibbonBar version: %1"
-                                "\n https://gitee.com/icanpool/QxRibbon"
-                                "\n ===============")
-                                 .arg(getRibbonVersion()));
+    QString url("https://gitee.com/icanpool/QxRibbon");
+
+    AboutDialog ad(QLatin1String(":/icon/res/logo.png"), tr("QxRibbon"), QLatin1String(getRibbonVersion()),
+                   tr(
+"Copyleft (C) 2023 maminjie &lt;canpool@163.com&gt;<br/>"
+"<br/>QxRibbon is a Qt componet like to Microsoft Ribbon style. "
+"For more information, please visit <a href=\"%1\">%1</a>").arg(url)
+                   );
+    ad.exec();
 }
 
 void MainWindow::onActionRemoveAppBtnTriggered(bool b)

commit 446e2cb3324f896f222d883c22703759f10c710a

qt6: fix build error for AboutDialog
Fixes: 329725a ("add AboutDialog")

Signed-off-by: canpool <canpool@163.com>

demo/aboutdialog.cpp
@@ -61,7 +61,11 @@ AboutDialog::AboutDialog(const QString &softlogo, const QString &softname, const
     layout->setSizeConstraint(QLayout::SetFixedSize);
 
     QString versionString = tr("%1 %2").arg(softname, softver);
+#if QT_VERSION < QT_VERSION_CHECK(5, 8, 0)
     QDateTime dt = QDateTime::fromTime_t((uint)getDateFromMacro(__DATE__));
+#else
+    QDateTime dt = QDateTime::fromMSecsSinceEpoch((qint64)getDateFromMacro(__DATE__) * 1000);
+#endif
     QString buildDateInfo = tr("<br/>Built on %1 %2<br/>").arg(dt.toString("yyyy-MM-dd"), __TIME__);
     QString buildCompatibilityString = tr("Based on Qt %1 (%2, %3 bit)").arg(QLatin1String(qVersion()),
                                                                              compilerString(),